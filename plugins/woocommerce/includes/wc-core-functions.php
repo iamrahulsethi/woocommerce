6865e8d2642204a01dd54e2a5c3594834f24d11b
Rahul Sethi <iamrahulsethi@users.noreply.github.com>
2023-06-04 23:46:02 +0530

    Update wc-core-functions.php

--- plugins/woocommerce/includes/wc-core-functions.php
+++ plugins/woocommerce/includes/wc-core-functions.php
@@ -2608,3 +2608,34 @@ function wc_cache_get_multiple( $keys, $group = '', $force = false ) {
 	}
 	return $values;
 }
+
+// Total products count
+
+
+
+function get_total_number_of_items_in_order( $order_id ) {
+    $order = wc_get_order( $order_id );
+    $total_quantity = 0;
+    foreach ( $order->get_items() as $item_id => $item ) {
+       $quantity = $item->get_quantity();
+       $total_quantity += $quantity;
+    }
+    return $total_quantity;
+}
+
+function display_total_quantity_in_order_details( $order_id ) {
+    $total_quantity = get_total_number_of_items_in_order( $order_id );
+    ?>
+
+            <tr>
+                <td class="label"><?php esc_html_e( 'Total Quantity:', 'woocommerce' ); ?></td>
+                <td width="1%"></td>
+                <td class="total">
+                 <?php echo htmlspecialchars($total_quantity); ?>
+                </td>
+            </tr>
+
+    <?php
+}
+
+add_action( 'woocommerce_admin_order_totals_after_discount', 'display_total_quantity_in_order_details', 10 );
